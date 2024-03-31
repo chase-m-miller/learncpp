4a3864fb6b69390c52926f26b08dbd057ebae6d8
Chase Miller <chasemmiller2@gmail.com>
2024-03-31 18:51:43 -0500

    Complete question 2 of test

---
+++ ch2test/io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number: ";
+    int num{};
+    std::cin >> num;
+
+    return num;
+}
+
+void writeAnswer(int answer)
+{
+    std::cout << "The answer is: " << answer << '\n';
+}
--- ch2test/main.cpp
+++ ch2test/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 
-int readNumber()
-{
-    std::cout << "Enter a number: ";
-    int num{};
-    std::cin >> num;
-
-    return num;
-}
-
-void writeAnswer(int answer)
-{
-    std::cout << "The answer is: " << answer << '\n';
-}
+int readNumber();
+void writeAnswer(int answer);
 
 int main()
 {
