84902ac84ff447dc9de7e6d7eafa55f37c3d5fe3
Chase Miller <chasemmiller2@gmail.com>
2024-03-31 18:47:48 -0500

    Add chapter 2 test program

---
+++ ch2test/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number: ";
+    int num{};
+    std::cin >> num;
+
+    return num;
+}
+
+void writeAnswer(int answer)
+{
+    std::cout << "The answer is: " << answer << '\n';
+}
+
+int main()
+{
+    int x {readNumber()};
+    int y {readNumber()};
+
+    writeAnswer(x + y);
+
+    return 0;
+}
