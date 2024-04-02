28aefeb6820786543748a57c2a84fb8ff3003144
Chase Miller <chasemmiller2@gmail.com>
2024-04-02 17:34:46 -0500

    Add debugging macro

--- ch2test/io.cpp
+++ ch2test/io.cpp
@@ -1,16 +1,27 @@
 #include "io.h"
 #include <iostream>
 
+#define ENABLE_DEBUG
+
 int readNumber()
 {
+#ifdef ENABLE_DEBUG
+std::cerr << "readNumber() called";
+#endif
     std::cout << "Enter a number: ";
     int num{};
     std::cin >> num;
+#ifdef ENABLE_DEBUG
+std::cerr << "Number input: " << num;
+#endif
 
     return num;
 }
 
 void writeAnswer(int answer)
 {
+#ifdef ENABLE_DEBUG
+std::cerr << "writeAnswer() called";
+#endif
     std::cout << "The answer is: " << answer << '\n';
 }
