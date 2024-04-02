ad15f50fcc88ad389ccaaca6bab9354935f02f3e
Chase Miller <chasemmiller2@gmail.com>
2024-04-02 17:37:01 -0500

    Append newline to debug statements

--- ch2test/io.cpp
+++ ch2test/io.cpp
@@ -6,13 +6,13 @@
 int readNumber()
 {
 #ifdef ENABLE_DEBUG
-std::cerr << "readNumber() called";
+std::cerr << "readNumber() called" << '\n';
 #endif
     std::cout << "Enter a number: ";
     int num{};
     std::cin >> num;
 #ifdef ENABLE_DEBUG
-std::cerr << "Number input: " << num;
+std::cerr << "Number input: " << num << '\n';
 #endif
 
     return num;
@@ -21,7 +21,7 @@ std::cerr << "Number input: " << num;
 void writeAnswer(int answer)
 {
 #ifdef ENABLE_DEBUG
-std::cerr << "writeAnswer() called";
+std::cerr << "writeAnswer() called" << '\n';
 #endif
     std::cout << "The answer is: " << answer << '\n';
 }
