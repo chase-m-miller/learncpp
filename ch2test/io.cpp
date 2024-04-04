6942c117914336b65890b8283625776ba4a1ebb9
Chase Miller <chasemmiller2@gmail.com>
2024-04-04 16:05:09 -0500

    Edit spacing for readability

--- ch2test/io.cpp
+++ ch2test/io.cpp
@@ -8,9 +8,11 @@ int readNumber()
 #ifdef ENABLE_DEBUG
 std::cerr << "readNumber() called" << '\n';
 #endif
+    
     std::cout << "Enter a number: ";
     int num{};
     std::cin >> num;
+
 #ifdef ENABLE_DEBUG
 std::cerr << "Number input: " << num << '\n';
 #endif
@@ -23,5 +25,6 @@ void writeAnswer(int answer)
 #ifdef ENABLE_DEBUG
 std::cerr << "writeAnswer() called" << '\n';
 #endif
+
     std::cout << "The answer is: " << answer << '\n';
 }
