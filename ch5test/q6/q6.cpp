4d67299e331e90960317d320965619738f54ba5f
Chase Miller <chasemmiller2@gmail.com>
2024-04-14 22:01:16 -0500

    Initialize question 6

---
+++ ch5test/q6/q6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Write the function getQuantityPhrase() here
+
+// Write the function getApplesPluralized() here
+
+int main()
+{
+    constexpr int maryApples { 3 };
+    std::cout << "Mary has " << getQuantityPhrase(maryApples) << ' ' << getApplesPluralized(maryApples) << ".\n";
+
+    std::cout << "How many apples do you have? ";
+    int numApples{};
+    std::cin >> numApples;
+
+    std::cout << "You have " << getQuantityPhrase(numApples) << ' ' << getApplesPluralized(numApples) << ".\n";
+
+    return 0;
+}
