9b17ae4aeea8ef88aa63b5ebb714952e5d808a72
Chase Miller <chasemmiller2@gmail.com>
2024-04-17 21:40:57 -0500

    Add factorial program

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -2,5 +2,19 @@
 
 constexpr int factorial(int x)
 {
-    
+    if (x == 1)
+        return 1;
+    else
+        return x * factorial(x - 1);
+}
+
+int main()
+{
+    std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << factorial(x) << '\n';
+
+    return 0;
 }
