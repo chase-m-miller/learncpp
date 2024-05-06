a5b58ed0740fc28344ad7584e2e9e1eb1de170a5
Chase Miller <chasemmiller2@gmail.com>
2024-05-06 02:15:24 -0500

    Add main function

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -15,7 +15,26 @@ int calculate(int x, int y, char operation)
     case '%':
         return x % y;
     default:
-        std::cout << "Invalid operator.\n";
-        return;
+        std::cout << "Invalid operator. Returning 0.\n";
+        return 0;
     }
+}
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    std::cout << "Enter a second integer: ";
+    int y{};
+    std::cin >> y;
+
+    std::cout << "Choose an operator (+, -, *, /, or %): ";
+    char operation{};
+    std::cin >> operation;
+
+    std::cout << calculate(x, y, operation) << '\n';
+
+    return 0;
 }
