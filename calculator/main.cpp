c8ac87377a53676c059302ab59435c8b8b483780
Chase Miller <chasemmiller2@gmail.com>
2024-05-06 02:07:21 -0500

    Add calculator function

---
+++ calculator/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int calculate(int x, int y, char operation)
+{
+    switch (operation)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    case '%':
+        return x % y;
+    default:
+        std::cout << "Invalid operator.\n";
+        return;
+    }
+}
