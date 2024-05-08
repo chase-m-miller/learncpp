688f13917ebb15633643272badff23bd71a0560c
Chase Miller <chasemmiller2@gmail.com>
2024-05-08 17:52:06 -0500

    Add uniform_int_distribution to produce dice rolls

--- mersenne-twister/main.cpp
+++ mersenne-twister/main.cpp
@@ -5,13 +5,16 @@ int main()
 {
     std::mt19937 mt{}; // instantiate a 32-bit mersenne twister
 
+    // create a RNG that generates uniform numbers between 1 and 6
+    std::uniform_int_distribution die6{ 1, 6 };
+
     // print random numbers
     for (int i{ 0 }; i <= 50; ++i)
     {
-        std::cout << mt() << '\t'; // generate a random number
+        std::cout << die6(mt) << '\t'; // generate a dice roll
 
         // start a new row if we've printed 5 numbers
-        if (i % 5 == 0)
+        if (i % 10 == 0)
         {
             std::cout << '\n';
         }
