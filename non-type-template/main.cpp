5c7c8b055897ef24051f32929c687af037e5ed60
Chase Miller <chasemmiller2@gmail.com>
2024-05-30 18:46:30 -0500

    Initialize question #1 from 11.9

---
+++ non-type-template/main.cpp
@@ -0,0 +1,12 @@
+// define your factorial() function template here
+
+int main()
+{
+    static_assert(factorial<0>() == 1);
+    static_assert(factorial<3>() == 6);
+    static_assert(factorial<5>() == 120);
+
+    factorial<-3>(); // should fail to compile
+
+    return 0;
+}
