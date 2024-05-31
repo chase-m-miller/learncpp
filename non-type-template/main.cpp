72359f988e6ecdfb6dd3eb5e83e0457aa8cfe13a
Chase Miller <chasemmiller2@gmail.com>
2024-05-31 02:05:39 -0500

    Complete factorial() function using non-type template

--- non-type-template/main.cpp
+++ non-type-template/main.cpp
@@ -1,4 +1,16 @@
-// define your factorial() function template here
+template <int N>
+constexpr int factorial()
+{
+    static_assert(N >= 0 && "Error: N must be greater than or equal to zero in factorial<N>()");
+
+    int result { 1 };
+    for (int i { N }; i > 0; --i)
+    {
+        result *= i;
+    }
+
+    return result;
+}
 
 int main()
 {
