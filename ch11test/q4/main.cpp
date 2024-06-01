fd9843ae7d9bcb85e6811cb88a7d386889302deb
Chase Miller <chasemmiller2@gmail.com>
2024-05-31 19:46:55 -0500

    Initialize q4 program

---
+++ ch11test/q4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// write your sub function template here
+
+int main()
+{
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+
+	return 0;
+}
