ec93b60f6f33fe7395d4461d1822386967d1a433
Chase Miller <chasemmiller2@gmail.com>
2024-06-07 19:23:57 -0500

    Add in paremeter vs out paremeter demonstration

---
+++ in-vs-out/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// in parameter
+int returnInt(int x)
+{
+    return x;
+}
+
+// out parameter
+void incrementInt(int* x)
+{
+    (*x)++;
+}
+
+int main()
+{
+    int x { 5 };
+
+    std::cout << returnInt(x) << '\n';
+
+    incrementInt(&x);
+    std::cout << x << '\n';
+
+    return 0;
+}
