4039326ba34b15d1497116a080ac6d8b307b2e9d
Chase Miller <chasemmiller2@gmail.com>
2024-06-18 03:02:53 -0500

    Add operator>> overload to assign enum value from user's input string

--- enum-to-string/main.cpp
+++ enum-to-string/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <optional>
 
 enum Color
 {
@@ -20,17 +23,59 @@ std::string_view getColor(Color color)
     }
 }
 
+std::optional<Color> getColorFromString(std::string_view sv)
+{
+    if (sv == "black") return black;
+    if (sv == "red")   return red;
+    if (sv == "blue")  return blue;
+    if (sv == "green") return green;
+
+    return {};
+}
+
 std::ostream& operator<<(std::ostream& out, Color color)
 {
     out << getColor(color);
     return out;
 }
 
+std::istream& operator>>(std::istream& in, Color& color)
+{
+    // Get a string from the user
+    std::string s {};
+    in >> s;
+
+    // See if string matches an enum, if it does we assign
+    // the enum to color and return the input stream
+    std::optional<Color> match { getColorFromString(s) };
+    if (match)
+    {
+        color = *match;
+        return in;
+    }
+
+    // Set input stream state to failed due to no match
+    in.setstate(std::ios_base::failbit);
+
+    return in;
+}
+
 int main()
 {
     Color shirt { red };
-    
     std::cout << "The color of your shirt is " << shirt << ".\n";
+
+    std::cout << "Enter black, red, blue, or green: ";
+    std::cin >> shirt;
+
+    if (std::cin)
+        std::cout << "You chose: " << shirt << '\n';
+    else
+    {
+        std::cin.clear(); // reset the input stream to good
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Your input was not valid\n";
+    }
 
     return 0;
 }
