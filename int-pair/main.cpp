76f77417fde73f931b545ffdaf49349b2d9da5fa
Chase Miller <chasemmiller2@gmail.com>
2024-06-26 12:43:21 -0500

    Add isEqual() member function

--- int-pair/main.cpp
+++ int-pair/main.cpp
@@ -10,6 +10,11 @@ struct IntPair
     {
         std::cout << "Pair(" << x << ", " << y << ")\n";
     }
+
+    bool isEqual(const IntPair& intPair)
+    {
+        return (x == intPair.x) && (y == intPair.y);
+    }
 };
 
 int main()
@@ -22,6 +27,9 @@ int main()
 
 	std::cout << "p2: ";
 	p2.print();
+
+    std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
 
 	return 0;
 }
