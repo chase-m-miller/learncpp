2857c83587f0f0bfa0b87ee8d39be6a02abb9279
Chase Miller <chasemmiller2@gmail.com>
2024-06-29 12:49:04 -0500

    Change Snake to class type (Note: broke program)

--- add-snake/oop/main.cpp
+++ add-snake/oop/main.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <string_view>
 
-struct Cat
+class Cat
 {
     std::string_view name{ "cat" };
     int numLegs{ 4 };
 };
 
-struct Dog
+class Dog
 {
     std::string_view name{ "dog" };
     int numLegs{ 4 };
 };
 
-struct Chicken
+class Chicken
 {
     std::string_view name{ "chicken" };
     int numLegs{ 2 };
 };
 
-struct Snake
+class Snake
 {
-    std::string_view name{ "snake" };
-    int numLegs{ 0 };
+public:
+
+    void printName()
+    {
+        std::cout << m_name;
+    }
+
+    void printNumLegs()
+    {
+        std::cout << m_numLegs;
+    }
+
+private:
+
+    std::string_view m_name{ "snake" };
+    int m_numLegs{ 0 };
 };
 
 int main()
 {
     constexpr Snake animal;
-    std::cout << "a " << animal.name << " has " << animal.numLegs << " legs\n";
+    std::cout << "a " << animal.printName() << " has " << animal.printNumLegs() << " legs\n";
 
     return 0;
 }
